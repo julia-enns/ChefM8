6ac73bd9a94f5fd2d2e0afa366ee90fef64f680f
Julia Grab <julia.grab@ucalgary.ca>
2022-03-29 18:27:07 -0600

    Added additional functionality to meal plan. Added additional prompts to add/edit recipes

--- ChefM8/Program.cs
+++ ChefM8/Program.cs
@@ -1,8 +1,10 @@
 using ChefM8;
+using ChefM8.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.Modal;
 using Blazored.LocalStorage;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredModal();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredToast();
+builder.Services.AddSingleton(new User());
 
 await builder.Build().RunAsync();
--- ChefM8/Shared/Recipe.cs
+++ ChefM8/Shared/Recipe.cs
@@ -1,6 +1,30 @@
 ﻿namespace ChefM8.Shared
 {
-    public record Recipe(string Name, string Link, MealCategories Category);
+    public record Recipe(string Name, string Link, string Image);
+    public class MealPlanRecipe {
+        public Recipe Recipe;
+        public Date? Date;
+        public MealCategories? Category;
+        public MealPlanRecipe(Recipe recipe, Date? date, MealCategories? category)
+        {
+            Recipe = recipe;
+            Date = date;
+            Category = category;
+        }
+    }
+
+    public class SelectedRecipe
+    {
+        public MealPlanRecipe Recipe;
+        public bool Selected;
+
+        public SelectedRecipe(MealPlanRecipe recipe, bool selected)
+        {
+            Recipe = recipe;
+            Selected = selected;
+        }
+    }
 
     public enum MealCategories { Breakfast, Lunch, Dinner, Other }
+    public enum Date { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
 }
