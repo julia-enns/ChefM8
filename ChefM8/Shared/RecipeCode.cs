7d7225abbed7600a40d5ce1f79da674272574720
VictorTDong <victortdong@gmail.com>
2022-04-04 17:01:51 -0600

    finished recipe pages along with functionality of the back button, dynamic ingredients list, video player and add to meal plan button. Add to grocery list not yet implemented as I dont know how that functionality is implemented yet

---
+++ ChefM8/Shared/RecipeCode.cs
@@ -0,0 +1,67 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using ChefM8.Pages.Recipes;
+using Microsoft.AspNetCore.Components;
+
+namespace ChefM8.Shared
+{
+    public abstract class RecipeCode : ComponentBase
+    {
+        public void AddToFavorites(User user, Recipe recipe)
+        {
+            MealPlanRecipe newMeal = new MealPlanRecipe(recipe, null, null);
+            if (!user.SavedRecipes.Contains(newMeal))
+            {
+                user.SavedRecipes.Add(newMeal);
+            }
+        } 
+        
+        public void showVideoPlayer(IModalService modal, int video)
+        {
+            var parameters = new ModalParameters();
+            parameters.Add(nameof(VideoPlayer.recipeNumber), video);
+            modal.Show<VideoPlayer>("", parameters, new ModalOptions() { Class = "custom-modal" });
+        }
+
+        public async void addRecipeToMealplanPopup(IModalService modal, User user, Recipe mealplanRecipe)
+        {
+            var parameters = new ModalParameters();
+            parameters.Add(nameof(AddToMealPlanPopup.recipe), new SelectedRecipe(new MealPlanRecipe(mealplanRecipe, null, null), true));
+            var formModal = modal.Show<AddToMealPlanPopup>("", parameters, new ModalOptions() { Class = "custom-modal" });
+            var result = await formModal.Result;
+
+            if (result.Data is null) return;
+
+            foreach (var recipe in (IList<MealPlanRecipe>)result.Data)
+            {
+                switch (recipe.Date)
+                {
+                    case Date.Sunday:
+                        user.SundayRecipes.Add(recipe);
+                        break;
+                    case Date.Monday:
+                        user.MondayRecipes.Add(recipe);
+                        break;
+                    case Date.Tuesday:
+                        user.TuesdayRecipes.Add(recipe);
+                        break;
+                    case Date.Wednesday:
+                        user.WednesdayRecipes.Add(recipe);
+                        break;
+                    case Date.Thursday:
+                        user.ThursdayRecipes.Add(recipe);
+                        break;
+                    case Date.Friday:
+                        user.FridayRecipes.Add(recipe);
+                        break;
+                    case Date.Saturday:
+                        user.SaturdayRecipes.Add(recipe);
+                        break;
+                    default:
+                        user.SavedRecipes.Add(recipe);
+                        break;
+                }
+            }
+        }
+    }
+}
