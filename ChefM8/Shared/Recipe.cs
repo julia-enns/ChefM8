435da99eaf69a32ebae687c5fc7a47ee7797f8b0
Kean <keandiether.arguelle@gitlab.cpsc.ucalgary.ca>
2022-04-07 05:48:33 -0600

    Favourite page, search page, and a chunk of the homepage implemented. But still far from finished

--- ChefM8/Shared/Recipe.cs
+++ ChefM8/Shared/Recipe.cs
@@ -1,6 +1,52 @@
 ﻿namespace ChefM8.Shared
 {
-    public record Recipe(string Name, string Link, MealCategories Category);
+    public enum Measurement { None, Tsp, Tbsp, Cup, Piece, Slice, Pound, Ounce }
+    public enum IngredientCategory { None, Bakery, Produce, Meats, Dairy, Baking, Other }
+    public record Ingredient(string Name, double Amount, Measurement Unit, IngredientCategory Category);
+    public enum Tags { Bakery, Produce, Pasta, Meats, Dairy, Baking, Beef, Vegetarian, Breakfast, Lunch, Dinner, Easy, Medium, Hard};
+
+    public record Recipe(string Name, string Link, string Image, List<Ingredient> Ingredients);
+    public class MealPlanRecipe {
+        public Recipe Recipe;
+        public Date? Date;
+        public MealCategories? Category;
+        public MealPlanRecipe(Recipe recipe, Date? date, MealCategories? category)
+        {
+            Recipe = recipe;
+            Date = date;
+            Category = category;
+        }
+    }
+
+    public class SelectedRecipe
+    {
+        public MealPlanRecipe Recipe;
+        public bool Selected;
+
+        public SelectedRecipe(MealPlanRecipe recipe, bool selected)
+        {
+            Recipe = recipe;
+            Selected = selected;
+        }
+    }
+
+    // Available recipes that would show up in the homepage
+    public class AvailableRecipes
+    {
+        public Recipe Recipe;
+        public List<Tags> Tags;
+        public string Description;
+        public string Duration;
+
+        public AvailableRecipes(Recipe recipe, List<Tags> tags, string desc, string duration)
+        {
+            Recipe = recipe;
+            Tags = tags;
+            Description = desc;
+            Duration = duration;
+        }
+    }
 
     public enum MealCategories { Breakfast, Lunch, Dinner, Other }
+    public enum Date { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
 }
---
+++ ChefM8/Shared/User.cs
@@ -0,0 +1,34 @@
+﻿namespace ChefM8.Shared
+{
+    public class User
+    {
+        public List<Ingredient> Groceries { get; set; }
+
+        public IList<AvailableRecipes> FavouriteRecipes { get; set; }
+
+        public IList<MealPlanRecipe> SavedRecipes { get; set; }
+        public IList<MealPlanRecipe> SundayRecipes { get; set; }
+        public IList<MealPlanRecipe> MondayRecipes { get; set; }
+        public IList<MealPlanRecipe> TuesdayRecipes { get; set; }
+        public IList<MealPlanRecipe> WednesdayRecipes { get; set; }
+        public IList<MealPlanRecipe> ThursdayRecipes { get; set; }
+        public IList<MealPlanRecipe> FridayRecipes { get; set; }
+        public IList<MealPlanRecipe> SaturdayRecipes { get; set; }
+
+        public User()
+        {
+            Groceries = new List<Ingredient>();
+
+            FavouriteRecipes = new List<AvailableRecipes>();
+
+            SavedRecipes = new List<MealPlanRecipe>();
+            SundayRecipes = new List<MealPlanRecipe>();
+            MondayRecipes = new List<MealPlanRecipe>();
+            TuesdayRecipes = new List<MealPlanRecipe>();
+            WednesdayRecipes = new List<MealPlanRecipe>();
+            ThursdayRecipes = new List<MealPlanRecipe>();
+            FridayRecipes = new List<MealPlanRecipe>();
+            SaturdayRecipes = new List<MealPlanRecipe>();
+        }
+    }
+}
