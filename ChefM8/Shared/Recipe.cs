1280af57b72dd2b7c44e1485d49ecffd66f75305
Kean <keandiether.arguelle@gitlab.cpsc.ucalgary.ca>
2022-04-11 10:25:24 -0600

    Fixed design for all recipes page. Favourite button now works. Filter tab still wonky. Discover section and search tab not done

--- ChefM8/Shared/Recipe.cs
+++ ChefM8/Shared/Recipe.cs
@@ -3,8 +3,17 @@
     public enum Measurement { None, Tsp, Tbsp, Cup, Piece, Slice, Pound, Ounce }
     public enum IngredientCategory { None, Bakery, Produce, Meats, Dairy, Baking, Other }
     public record Ingredient(string Name, double Amount, Measurement Unit, IngredientCategory Category);
-    public enum Tags { Bakery, Produce, Pasta, Meats, Dairy, Baking, Beef, Vegetarian, Breakfast, Lunch, Dinner, Easy, Medium, Hard};
-
+    // anything after "hard" are fillers
+    public enum Tags { Bakery, Produce, Pasta, Meats, Dairy, Baking, Beef, Vegetarian, Breakfast, Lunch, Dinner, Dessert, Easy, Medium, Hard, Gluten_Free, Low_Carb, Keto, Nut_Free,
+        Chicken,
+        Fish,
+        Pork,
+        Fall,
+        Holiday,
+        Spring,
+        Summer,
+        Winter
+    }
     public record Recipe(string Name, string Link, string Image, List<Ingredient> Ingredients);
     public class MealPlanRecipe {
         public Recipe Recipe;
@@ -47,20 +56,35 @@
         }
     }
 
-    // Recipes that are not functional and only used for display
-    public class DisplayRecipes
+    public class FilterCategory
     {
-
-        public List<AvailableRecipes> Recipes;
+        public string Category;
+        public List<Tags> Filters;
 
         // constructor populates the list of display reci
-        public DisplayRecipes()
+        public FilterCategory(string category, List<Tags> filters)
         {
-            
+            Category = category;
+            Filters = filters;
         }
     }
 
+    public class AllFilters
+    {
+        public List<FilterCategory> Filters;
 
+        public AllFilters()
+        {
+            Filters = new List<FilterCategory>();
+
+            Filters.Add(new FilterCategory("Meal", new List<Tags> { Tags.Breakfast, Tags.Lunch, Tags.Dinner, Tags.Dessert }));
+            Filters.Add(new FilterCategory("Difficulty", new List<Tags> { Tags.Easy, Tags.Medium, Tags.Hard}));
+            Filters.Add(new FilterCategory("Diet", new List<Tags> { Tags.Meats, Tags.Vegetarian, Tags.Gluten_Free, Tags.Nut_Free, Tags.Keto, Tags.Low_Carb}));
+            Filters.Add(new FilterCategory("Meat", new List<Tags> { Tags.Beef, Tags.Chicken, Tags.Fish, Tags.Pork}));
+            Filters.Add(new FilterCategory("Ingredient Type", new List<Tags> { Tags.Meats, Tags.Dairy, Tags.Pasta, Tags.Vegetarian }));
+            Filters.Add(new FilterCategory("Season", new List<Tags> { Tags.Fall, Tags.Holiday, Tags.Spring, Tags.Summer, Tags.Winter }));
+        }
+    }
 
 
     public enum MealCategories { Breakfast, Lunch, Dinner, Other }
