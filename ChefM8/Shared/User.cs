ad41efa671cb2c1011db8d846fb8a8c85d89461c
Julia Grab <julia.grab@ucalgary.ca>
2022-04-12 13:29:38 -0600

    Added some logic to fill the grocery list with ingredients

--- ChefM8/Shared/User.cs
+++ ChefM8/Shared/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public List<Ingredient> Groceries { get; set; }
+        public IList<Ingredient> Groceries { get; set; }
 
         public IList<MealPlanRecipe> SavedRecipes { get; set; }
         public IList<MealPlanRecipe> SundayRecipes { get; set; }
@@ -15,7 +15,17 @@
 
         public User()
         {
-            Groceries = new List<Ingredient>();
+            Groceries = new List<Ingredient>() {
+                new Ingredient("Ground beef", 1, Measurement.Pound, IngredientCategory.Meats),
+                new Ingredient("Dried spaghetti", 2, Measurement.Ounce, IngredientCategory.Other),
+                new Ingredient("Shredded parmesan cheese", 1, Measurement.Cup, IngredientCategory.Dairy),
+                new Ingredient("Tomato paste", 1, Measurement.Tbsp, IngredientCategory.Other),
+                new Ingredient("Can crushed tomatoes", 1, Measurement.None, IngredientCategory.Other),
+                new Ingredient("Garlic cloves", 1, Measurement.None, IngredientCategory.Produce),
+                new Ingredient("Olive oil", 3, Measurement.Tbsp, IngredientCategory.Other),
+                new Ingredient("Salt", 1, Measurement.Tsp, IngredientCategory.Other),
+                new Ingredient("Pepper", 1, Measurement.Tsp, IngredientCategory.Other)
+                };
 
             SavedRecipes = new List<MealPlanRecipe>();
             SundayRecipes = new List<MealPlanRecipe>();
